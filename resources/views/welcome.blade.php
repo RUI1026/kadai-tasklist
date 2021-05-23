bbb40692468eb617e9bf8fcff923eccad6d1f7aa
rui.morita <mori_morita@hotmail.co.jp>
2021-05-23 03:58:44 +0000

    set timezone

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    TaskList
                 </div>
 
                 <div class="links">
